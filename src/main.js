64080b1dc1c672bb19b2d9699aee14bb4f8981ea
bdurb <brian@rpmgreenville.com>
2019-03-19 13:55:58 -0700

    imported highcharts into main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
+import HighchartsVue from 'highcharts-vue'
 import App from './App.vue'
 
+Vue.use(Highcharts, {tagName: 'charts'})
 Vue.config.productionTip = false
 
 new Vue({
