a5681358c3d7b3d756bf8ddfd71767a120d3b633
bdurb <brian@rpmgreenville.com>
2019-03-22 16:55:54 -0700

    set up boilerplate for ajax requests

--- src/components/ExampleTwo.vue
+++ src/components/ExampleTwo.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-
+    <h1>Placeholder</h1>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
   export default {
-    
+    data() {
+      return {
+        response: ''
+      }
+    }
   }
 </script>
 
